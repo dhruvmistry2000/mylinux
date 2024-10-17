fc91f3d89d01d6832e576b23ef7b32a7fedaa733
Dhruv Mistry <dhruvmistry2000@gmail.com>
2024-10-17 15:24:41 +0530

    theme adding

--- setup.sh
+++ setup.sh
@@ -104,11 +104,11 @@ installDepend() {
     ## Check for dependencies.
       ## Check for dependencies based on the package manager.
     if [ "$PACKAGER" = "pacman" ]; then
-        DEPENDENCIES='arandr bat btop tree xarchiver flameshot fastfetch wget unzip bspwm dconf dunst kitty nautilus thunar-volman thunar-archive-plugin nitrogen picom git pulseaudio-alsa pulseaudio-bluetooth ly yazi bluez bluez-utils brightnessctl htop npm python3-pip libconfig dbus libev meson pcre2 pixman uthash xcb-util-image xcb-util-renderutil cmake libxft libimlib2 libxinerama libxcb-res xorg-xev xorg-xbacklight alsa-utils rofi polybar sxhkd tldr nwg-look fzf'
+        DEPENDENCIES='arandr bat btop tree xarchiver flameshot fastfetch wget unzip bspwm dconf dunst kitty nautilus thunar-volman thunar-archive-plugin nitrogen picom git pulseaudio-alsa pulseaudio-bluetooth ly yazi bluez bluez-utils brightnessctl htop npm python3-pip libconfig dbus libev meson pcre2 pixman uthash xcb-util-image xcb-util-renderutil cmake libxft libimlib2 libxinerama libxcb-res xorg-xev xorg-xbacklight alsa-utils rofi polybar sxhkd tldr nwg-look fzf materia-gtk-theme'
     elif [ "$PACKAGER" = "nala" ] || [ "$PACKAGER" = "apt" ]; then
-        DEPENDENCIES='bspwm sxhkd btop picom xarchiver flameshot kitty polybar rofi thunar thunar-archive-plugin thunar-volman nitrogen htop brightnessctl dunst git cmake meson npm python3 python3-pip dconf tldr fontconfig nwg-look fzf'
+        DEPENDENCIES='bspwm sxhkd btop picom xarchiver flameshot kitty polybar rofi thunar thunar-archive-plugin thunar-volman nitrogen htop brightnessctl dunst git cmake meson npm python3 python3-pip dconf tldr fontconfig nwg-look fzf materia-gtk-theme'
     elif [ "$PACKAGER" = "dnf" ]; then
-        DEPENDENCIES='bspwm sxhkd btop picom xarchiver flameshot kitty polybar rofi thunar thunar-archive-plugin thunar-volman nitrogen htop brightnessctl dunst git gcc cmake meson npm python3 python3-pip dconf tldr fontconfig nwg-look fzf'
+        DEPENDENCIES='bspwm sxhkd btop picom xarchiver flameshot kitty polybar rofi thunar thunar-archive-plugin thunar-volman nitrogen htop brightnessctl dunst git gcc cmake meson npm python3 python3-pip dconf tldr fontconfig nwg-look fzf materia-gtk-theme'
     else
         printf "${RED}Unsupported package manager: $PACKAGER${RC}\n"
         exit 1
