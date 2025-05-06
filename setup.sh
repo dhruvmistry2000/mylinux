06a62bc7c688bc0a9375121970078d78eb3bc1ab
Dhruv Mistry <dhruvmistry2000@gmail.com>
2025-05-06 16:51:48 +0530

    Adding ncdu

--- setup.sh
+++ setup.sh
@@ -104,11 +104,11 @@ installDepend() {
     ## Check for dependencies.
       ## Check for dependencies based on the package manager.
     if [ "$PACKAGER" = "pacman" ]; then
-        DEPENDENCIES='arandr xorg ntfs-3g bat btop tree xarchiver flameshot fastfetch wget nvtop unzip bspwm dconf dunst kitty nautilus thunar-volman thunar-archive-plugin nitrogen picom git ly yazi bluez bluez-utils brightnessctl htop npm python3-pip libconfig dbus libev meson pcre2 pixman uthash xcb-util-image xcb-util-renderutil cmake libxft libimlib2 libxinerama libxcb-res xorg-xev xorg-xbacklight alsa-utils rofi polybar sxhkd tldr vim nwg-look fzf materia-gtk-theme'
+        DEPENDENCIES='arandr xorg ntfs-3g bat btop tree ncdu xarchiver flameshot fastfetch wget nvtop unzip bspwm dconf dunst kitty nautilus thunar-volman thunar-archive-plugin nitrogen picom git ly yazi bluez bluez-utils brightnessctl htop npm python3-pip libconfig dbus libev meson pcre2 pixman uthash xcb-util-image xcb-util-renderutil cmake libxft libimlib2 libxinerama libxcb-res xorg-xev xorg-xbacklight alsa-utils rofi polybar sxhkd tldr vim nwg-look fzf materia-gtk-theme'
     elif [ "$PACKAGER" = "nala" ] || [ "$PACKAGER" = "apt" ]; then
-        DEPENDENCIES='bspwm xorg sxhkd btop picom xarchiver flameshot kitty polybar rofi thunar nvtop thunar-archive-plugin thunar-volman nitrogen htop brightnessctl dunst git cmake meson npm python3 python3-pip  fontconfig fzf vim materia-gtk-theme'
+        DEPENDENCIES='bspwm xorg sxhkd btop picom xarchiver ncdu flameshot kitty polybar rofi thunar nvtop thunar-archive-plugin thunar-volman nitrogen htop brightnessctl dunst git cmake meson npm python3 python3-pip  fontconfig fzf vim materia-gtk-theme'
     elif [ "$PACKAGER" = "dnf" ]; then
-        DEPENDENCIES='bspwm xorg sxhkd btop picom xarchiver flameshot kitty polybar rofi thunar nvtop thunar-archive-plugin thunar-volman nitrogen htop brightnessctl dunst git gcc cmake meson npm python3 python3-pip dconf tldr vim fontconfig nwg-look fzf materia-gtk-theme'
+        DEPENDENCIES='bspwm xorg sxhkd btop picom xarchiver ncdu flameshot kitty polybar rofi thunar nvtop thunar-archive-plugin thunar-volman nitrogen htop brightnessctl dunst git gcc cmake meson npm python3 python3-pip dconf tldr vim fontconfig nwg-look fzf materia-gtk-theme'
     else
         printf "${RED}Unsupported package manager: $PACKAGER${RC}\n"
         exit 1
